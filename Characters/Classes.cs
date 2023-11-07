073884735bddc0119ec990212595def572b24ca0
Milton Österberg <miltonwendel@outlook.com>
2023-11-07 12:20:02 +0100

    Sortera

--- Characters/Classes.cs
+++ Characters/Classes.cs
@@ -15,54 +15,7 @@ abstract class Character
     }
 }
 
-class Hero : Character
-{
-    public double Experience {get; set;}
-    public int Level {get; set;}
-    public List<Inventory> inventory;
-    public Hero(double experience, int level, double damage, double hp, int armor, int affinity) : base(damage, hp, armor, affinity)
-    {
-        Experience = experience;
-        Level = level;
-    }
-    public void Engage()
-    {
 
-    }
-    public void Dialog()
-    {
 
-    }
-    public void InventoryManagement()
-    {
 
-    }
-    public void Map()
-    {
 
-    }
-}
-
-class Minion : Character
-{
-    public Minion(double damage, double hp, int armor, int affinity) : base(damage, hp, armor, affinity)
-    {
-
-    }
-    public void Engage()
-    {
-
-    }
-}
-
-class Friendly : Character
-{
-    public Friendly(double damage, double hp, int armor, int affinity) : base(damage, hp, armor, affinity)
-    {
-
-    }
-    public void Dialog()
-    {
-
-    }
-}
---
+++ Characters/Minion.cs
@@ -0,0 +1,11 @@
+class Minion : Character
+{
+    public Minion(double damage, double hp, int armor, int affinity) : base(damage, hp, armor, affinity)
+    {
+
+    }
+    public void Engage()
+    {
+
+    }
+}
