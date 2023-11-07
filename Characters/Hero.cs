1c4ef519e447c4468db30a18a5d12054c6d4b43a
Milton Österberg <miltonwendel@outlook.com>
2023-11-07 16:43:07 +0100

    nu har hero ett inventory som abilities ligger i

--- Characters/Hero.cs
+++ Characters/Hero.cs
@@ -1,13 +1,16 @@
 
+using System.Configuration.Assemblies;
+
 class Hero : Character
 {
     public double Experience {get; set;}
     public int Level {get; set;}
-    public List<Inventory> inventory;
+    public List<Ability> inventory;
     public Hero(double experience, int level, string name, double damage, double hp, int armor, int affinity) : base(name, damage, hp, armor, affinity)
     {
         Experience = experience;
         Level = level;
+        inventory = new List<Ability>();
     }
     public double Engage(Character enemy, Hero playerCharacter)
     {
@@ -17,9 +20,17 @@ class Hero : Character
     {
 
     }
-    public void InventoryManagement()
+    public List<Ability> Inventory()
     {
-
+        return inventory;
+    }
+     public void AddInventory(Ability ability)
+    {
+        inventory.Add(ability);
+    }
+    public void RemoveInventory(Ability ability)
+    {
+        inventory.Remove(ability);
     }
     public void Map()
     {
